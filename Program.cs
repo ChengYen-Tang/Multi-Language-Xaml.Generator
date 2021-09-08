1f8d21d677aba38c237238252fe80fa18f5d70a1
Kenneth Tang <kenneth@nssh.onmicrosoft.com>
2021-09-08 12:13:04 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Multi_Language_Xaml.Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
